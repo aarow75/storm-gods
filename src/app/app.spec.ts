25a2f8ebfbd92732e195d1b6df07533acd0461ac
Aaron Swensen <aaron.swensen@gmail.com>
2026-06-21 08:08:08 -0600

    fix tests

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -16,10 +16,12 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should render logo in header', async () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('RuneQuest Character Generator');
+    const logo = compiled.querySelector('h1 img') as HTMLImageElement;
+    expect(logo).toBeTruthy();
+    expect(logo.src).toContain('logo.png');
   });
 });
